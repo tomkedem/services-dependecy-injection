2b641109e5e1ba59307a2dc3ff40bd930956fdf0
Tomer Kedem <tompha1@hotmail.com>
2024-11-29 20:10:19 +0200

    Outsourcing & Reusing Logic with Services

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,7 @@
-import { Component, ElementRef, viewChild } from '@angular/core';
+import { Component, ElementRef, inject, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TasksService } from '../tasks.service';
+import { TaskStatus } from '../task.model';
 
 @Component({
   selector: 'app-new-task',
@@ -10,8 +12,12 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
+  private taskService = inject(TasksService);
 
   onAddTask(title: string, description: string) {
+    this.taskService.addTask({title, description})
     this.formEl()?.nativeElement.reset();
   }
+
+  
 }
