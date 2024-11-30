27ff06cc239a010fbdc152be62d98db112ed407d
Tomer Kedem <tompha1@hotmail.com>
2024-11-30 07:16:12 +0200

    Injecting Other Values (NOT Services)

--- src/app/tasks/task.model.ts
+++ src/app/tasks/task.model.ts
@@ -1,10 +1,19 @@
+import { InjectionToken, Provider } from "@angular/core";
+
 export type TaskStatus = 'OPEN' | 'IN_PROGRESS' | 'DONE';
 
-export const TaskStatusOptions: {
+type TaskStatusOptions = {
   value: 'open' | 'in-progress' | 'done';
   taskStatus: string;
   text: string;
-}[] = [
+}[];
+
+export const TASK_STATUS_OPTIONS = new InjectionToken<TaskStatusOptions>(
+  'task-status-token'
+);
+
+export const TaskStatusOptions: TaskStatusOptions=
+ [
   {
     value: 'open',
     taskStatus: 'OPEN',
@@ -21,6 +30,12 @@ export const TaskStatusOptions: {
     text: 'Completed'
   },
 ]
+
+export const taskStatusOptionsProvider: Provider = 
+{
+  provide: TASK_STATUS_OPTIONS,
+  useValue: TaskStatusOptions
+}
 
 export interface Task {
   id: string;
--- src/app/tasks/tasks-list/task-item/task-item.component.ts
+++ src/app/tasks/tasks-list/task-item/task-item.component.ts
@@ -1,7 +1,7 @@
 import { Component, computed, inject, input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { Task, TaskStatus } from '../../task.model';
+import { Task, TASK_STATUS_OPTIONS, TaskStatus } from '../../task.model';
 import { TasksService } from '../../tasks.service';
 import { TasksServiceToken } from '../../../../main';
 
@@ -15,6 +15,7 @@ import { TasksServiceToken } from '../../../../main';
 export class TaskItemComponent {
 
   private tasksService = inject(TasksServiceToken);
+  taskStatusOptions = inject(TASK_STATUS_OPTIONS)
   task = input.required<Task>();
   taskStatus = computed(() => {
     switch (this.task().status) {
